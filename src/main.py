b5683e658d4a2caca1319f36eaee6dda41d9287d
Vasyl Poremchuk <nleveryone@gmail.com>
2023-06-17 17:59:56 +0300

    Add a middleware

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.routers import admin, address, auth, todo, user
 
 app = FastAPI(title="Todo")
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router=address.router)
 app.include_router(router=admin.router)
